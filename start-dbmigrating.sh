4f29a3ca3af5cb798fe1c61f13576814a367f2ff
marcflohrer <59135776+marcflohrer@users.noreply.github.com>
2021-03-28 20:57:28 +0200

    fix db migration entrypoint

--- start-dbmigrating.sh
+++ start-dbmigrating.sh
@@ -2,6 +2,6 @@
 cd Data/Generated/ && find . ! -name . -prune ! -name ../Models -exec rm {} \; && cd ../..
 dotnet user-secrets set ConnectionStrings:DefaultConnection "Server=db;Database=master;User=sa;Password=${DatabasePassword};" --project .
 dotnet user-secrets set DatabasePassword "${DatabasePassword}" --project .
-docker-compose -f docker-compose-dbmigrating.yml up --build --remove-orphans 
+docker-compose -f docker-compose-dbmigrate.yml up --build --remove-orphans 
 dotnet user-secrets remove "DatabasePassword"
 dotnet user-secrets remove "ConnectionStrings:DefaultConnection"
