f20ab618eca654607520d83189eda3b54b751a7a
marcflohrer <59135776+marcflohrer@users.noreply.github.com>
2021-04-10 20:35:03 +0200

    add sample test

---
+++ test/AsyncExamples.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace my_demo_app_tests
+{
+    public class AsyncExamples
+    {
+        internal async void CodeThrowsAsync()
+        {
+            Task testCode() => Task.Factory.StartNew(ThrowingMethod);
+
+            var ex = await Assert.ThrowsAsync<NotImplementedException>(testCode);
+
+            Assert.IsType<NotImplementedException>(ex);
+        }
+
+        [Fact]
+        public async void RecordAsync()
+        {
+            Task testCode() => Task.Factory.StartNew(ThrowingMethod);
+
+            var ex = await Record.ExceptionAsync(testCode);
+
+            Assert.IsType<NotImplementedException>(ex);
+        }
+
+        void ThrowingMethod()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
